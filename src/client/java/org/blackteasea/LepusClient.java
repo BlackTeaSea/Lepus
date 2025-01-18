c9614b9316bf0669dffbd2ed273872c077038374
Jonathan Jara <jonathan.jara.2000@gmail.com>
2025-01-17 16:33:57 -0800

    Fabric Template 😝

---
+++ src/client/java/org/blackteasea/LepusClient.java
@@ -0,0 +1,10 @@
+package org.blackteasea;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LepusClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/org/blackteasea/LepusDataGenerator.java
@@ -0,0 +1,11 @@
+package org.blackteasea;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LepusDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
