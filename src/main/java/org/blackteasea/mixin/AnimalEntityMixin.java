380ae3b45003f970d3b44fb86d32c54d9e9790eb
Jonathan Jara <jonathan.jara.2000@gmail.com>
2025-01-18 22:13:58 -0800

    Made AnimalEntityMixin which reads the interactions of animals with humans

---
+++ src/main/java/org/blackteasea/mixin/AnimalEntityMixin.java
@@ -0,0 +1,28 @@
+package org.blackteasea.mixin;
+
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AnimalEntity.class)
+public class AnimalEntityMixin {
+
+    private static final String MOD_ID = "lepus";
+    private static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Inject(at = @At("HEAD"), method = "interactMob", cancellable = true)
+    private void onInteractMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        if (player.getMainHandStack().getItem() == Items.CARROT) {
+            LOGGER.info("Carrot interacted with animal");
+            cir.setReturnValue(ActionResult.SUCCESS);
+        }
+    }
+}
