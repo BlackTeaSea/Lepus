43fd80b81caa86be09b343d0486476435d82f1df
Jonathan Jara <jonathan.jara.2000@gmail.com>
2025-01-18 23:13:44 -0800

    Created Rabbit interaction

--- src/main/java/org/blackteasea/mixin/AnimalEntityMixin.java
+++ src/main/java/org/blackteasea/mixin/AnimalEntityMixin.java
@@ -2,7 +2,6 @@ package org.blackteasea.mixin;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.entity.passive.RabbitEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
